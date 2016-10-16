d181a9acc0ef81d20f2dfc743df6e0cbca6a1481
Max Enko <maxostap@gmail.com>
2016-10-16 15:26:24 -0700

    Second level grid dev.
    Adding detection of cone of vision to parametric level construction.

--- Private/HexTileBase.cpp
+++ Private/HexTileBase.cpp
@@ -1,25 +1,34 @@
 // Copyright 2015 - Max Enko
 
 #include "MachRace.h"
+#include "DrawDebugHelpers.h"
 #include "HexTileBase.h"
 
 
+void AHexTileBase::Tick(float DeltaSeconds) {
+	Super::Tick(DeltaSeconds);
+
+	if (DrawDebug) {
+		drawDebug();
+	}
+}
 
 void AHexTileBase::projectGrid() {
 
 	float cellHeight = CellSize * 2;
-	float cellWidth = FMath::Sqrt(3) / 2 * cellHeight;
+	float cellWidth = FMath::Sqrt(3) / 1.5 * cellHeight;
 
 	int32 rows = FMath::FloorToInt(VerticalRange / cellHeight);
 	int32 cols = FMath::FloorToInt(HorisontalRange / cellWidth);
+	float yOffset = cols * cellWidth/2; // offset grid to make origin in the middle
 
 	auto generateCellT = [&](int32 r, int32 c) {
 
 		float x = r * cellHeight;
-		float y = c * cellWidth;
-		
+		float y = c * cellWidth + (r % 2 == 1 ? cellWidth/2 : 0); // offset each odd row
+
 		return FTransform(
-			FVector(x, y, 0)
+			FVector(x, y - yOffset, 0)
 		);
 	};
 
@@ -29,12 +38,57 @@ void AHexTileBase::projectGrid() {
 
 			// generate a cell transform
 			this->Grid.Add(generateCellT(r,c));
+
+		}
+	}
+}
+
+
+bool AHexTileBase::isWithinThreshold(FVector v) {
+	
+	if (!Target) {
+		return false;
+	}
+
+	auto cm = UGameplayStatics::GetPlayerCameraManager(this, 0);
+	
+	auto forward = cm->GetCameraRotation().Vector(); // look at normal
+	FVector directional = Target->GetActorLocation() - v; // to target normal
+
+	forward.Normalize();
+	directional.Normalize();
+	
+	float dProductAngle = FMath::RadiansToDegrees(FMath::Acos(FVector::DotProduct(forward, directional))); // angle to target
+
+	return FMath::Abs(VisibleAngleThreshold) > FMath::Abs(180-dProductAngle);
+}
+
+void AHexTileBase::drawDebug() {
+	for (FTransform t : Grid) {
+		if (isWithinThreshold(t.GetLocation())) {
+			DrawDebugPoint(GetWorld(), t.GetLocation(), 30, FColor::Red, false, .08, 0);
 		}
 	}
 }
 
 void AHexTileBase::GenerateCoordinateGrid() {
 	this->projectGrid();
-
 }
 
+TArray<float> AHexTileBase::CalcDistancesToTarget() {
+
+	if (!Target || Grid.Num() == 0) {
+		return TArray<float>();
+	}
+
+	TArray<float> distances;
+
+	FVector targetWorldPos = Target->GetActorLocation();
+
+	for (FTransform t : Grid) {
+		distances.Add(FVector::Dist(targetWorldPos, t.GetLocation()));
+		//isWithinThreshold(t.GetLocation());
+	}
+
+	return distances;
+}
--- Public/HexTileBase.h
+++ Public/HexTileBase.h
@@ -9,27 +9,45 @@
  * 
  */
 UCLASS()
-class MACHRACE_API AHexTileBase : public ARaceActorBase
-{
+class MACHRACE_API AHexTileBase : public ARaceActorBase {
 	GENERATED_BODY()
 	
 private:
 	void projectGrid();
+	bool isWithinThreshold(FVector v); // checks to see if two coordinates are within threshold specified by SpawnThreshold
+	void drawDebug();
 
 public:
-	UPROPERTY(BlueprintReadOnly, Category = "MachRace|Gameplay")
+	UPROPERTY(BlueprintReadOnly, Category = "MachRace|Grid")
 	TArray<FTransform> Grid;
 
-	UPROPERTY(BlueprintReadOnly, Category = "MachRace|Gameplay")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MachRace|Grid")
 	float HorisontalRange = 10000;
 
-	UPROPERTY(BlueprintReadOnly, Category = "MachRace|Gameplay")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MachRace|Grid")
 	float VerticalRange = 10000;
 
-	UPROPERTY(BlueprintReadOnly, Category = "MachRace|Gameplay")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MachRace|Grid")
 	float CellSize = 80;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MachRace|System")
+	AActor* Target = NULL;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MachRace|System")
+	FVector SpawnThreshold = FVector(500,1000,0);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MachRace|System")
+	float VisibleAngleThreshold = 40;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MachRace|Utility")
+	bool DrawDebug = false;
+
+	virtual void Tick(float DeltaSeconds) override;
 
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Generate Grid Coordinates", Keywords = "Generate hexagonal grid transforms."), Category = "MachRace|Grid")
 	void GenerateCoordinateGrid();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "Calculate distances to Target", Keywords = "Calculates distances to target from each Grid transform. Worldspace."), Category = "MachRace|Grid")
+	TArray<float> CalcDistancesToTarget();
 
 };
