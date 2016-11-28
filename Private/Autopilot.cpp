dcf1e205b3b3618b0587d231d6991f1e713da5e6
Max Enko <maxostap@gmail.com>
2016-11-27 18:20:37 -0800

    follow speed with offset

--- Private/Autopilot.cpp
+++ Private/Autopilot.cpp
@@ -21,14 +21,74 @@ void UAutopilot::BeginPlay() {
 	Super::BeginPlay();
 
 	// ...
-	
 }
 
 
 // Called every frame
 void UAutopilot::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	lastDelta = DeltaTime;
+	UpdateVelocity();
+}
+
+
+FVector UAutopilot::getTargetVelocity() {
 	
+	// get current target actor velocity velocity (zero vector if not following or no target)
+	FVector currentTargetActorVelocity = Target && FollowTarget ? UX::GetRootLinearVelocity(Target) : FVector::ZeroVector;
+	FVector currentTargetActorVelocityN = currentTargetActorVelocity;
+	currentTargetActorVelocityN.Normalize();
+
+	FVector targetActorLoc = Target->GetActorLocation();
+	FVector ownerLoc = GetOwner()->GetActorLocation();
+
+	// factor in target offset location (this can be a moving location in front/behind/side of target)
+	TargetFollowLocation = FVector(FollowOffset, 0 , 0) + targetActorLoc; // follow location is just an X axis offset location
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// calculate follow velocity
+	//////////////////////////////////////////////////////////////////////////
+	if (FollowTarget) {
+		
+		// if offset location doesn't match, we need to adjust velocity slightly
+		// to get to that location
+		if (ownerLoc.X != TargetFollowLocation.X) {
+
+			// calc dist in X, clamp to offset to keep division no more than 1.0
+			float dist = FVector::Dist(FVector(ownerLoc.X, 0, 0), FVector(TargetFollowLocation.X, 0, 0));
+
+			// calc percentage of distance away from target follow location
+			float pctOfDistToTargetFollowLoc = FMath::Clamp<float>(dist/FMath::Abs(FollowOffset), 0, 1);
+
+			// multiplier for additional (accelerated) follow speed
+			float multiplier = FollowOffsetApproachCurve ? FollowOffsetApproachCurve->GetFloatValue(pctOfDistToTargetFollowLoc) : pctOfDistToTargetFollowLoc;
+			float multiplierMod = TargetFollowLocation.X < ownerLoc.X ? 1 : -1;
+
+			float additionalSpeed = MaxFollowSpeed * multiplier * multiplierMod;
+
+			TargetFollowVelocity = FVector(additionalSpeed, 0, 0);
+		}
+
+	} else {
+		TargetFollowVelocity = FVector::ZeroVector;
+	}
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// calculate dodge velocity
+	//////////////////////////////////////////////////////////////////////////
+	DodgeVelocity = FVector::ZeroVector;
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// calculate velocity to position owner in line (Y axis) with Target 
+	//////////////////////////////////////////////////////////////////////////
+	AlignWithTargetVelocity = FVector::ZeroVector;
+
+
+	return UX::GetRootLinearVelocity(Target) + TargetFollowVelocity + DodgeVelocity + AlignWithTargetVelocity;
 }
 
 
@@ -36,30 +96,21 @@ void UAutopilot::AdjustVelocityToFollowTarget(float delta) {
 	if (!Target) {
 		return;
 	}
-
-	// target location (Target Loc + FollowDistance)
-	FVector targetLoc = Target->GetActorLocation() + FVector(FllowDistance, 0, 0);
-
-	// distance between current location and target location in X axis only
-	float dist = FVector::Dist( 
-		FVector(Target->GetActorLocation().X,0,0), 
-		FVector(targetLoc.X, 0, 0 )
-		);
-
-
-
-	FVector currentVelocity = UX::GetRootLinearVelocity(Target);
-
-	UX::SetRootLinearVelocity(
-		Target,
-		FMath::VInterpTo(currentVelocity, Velocity, delta, NavigationSpeed)
-	);
+	
+	UX::SetRootLinearVelocity(GetOwner(), getTargetVelocity());
 }
 
 
-void UAutopilot::keepUpWithTarget() {
-	float targetFollowDist = Target->GetActorLocation().X + FllowDistance;
-	if (GetOwner()->GetActorLocation().X != targetFollowDist) {
+void UAutopilot::Scan() {
 
-	}
+}
+
+
+void UAutopilot::UpdateVelocity() {
+
+	TargetVelocity = getTargetVelocity();
+
+	FVector newV = FMath::VInterpTo(UX::GetRootLinearVelocity(GetOwner()), TargetVelocity, lastDelta, VelocityChangeSpeed);
+
+	UX::SetRootLinearVelocity(GetOwner(), newV);
 }
--- Public/Autopilot.h
+++ Public/Autopilot.h
@@ -6,6 +6,15 @@
 #include "Autopilot.generated.h"
 
 
+UENUM(BlueprintType)		//"BlueprintType" is essential to include
+enum class EDroneOperationType : uint8
+{
+	DroneOp_FollowTarget 			UMETA(DisplayName = "Follow Target"),
+	DroneOp_Dodge					UMETA(DisplayName = "Dodge Obstacles"),
+	DroneOp_AlignWithTarget			UMETA(DisplayName = "Get in front of Player")
+};
+
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class MACHRACE_API UAutopilot : public UActorComponent
 {
@@ -13,8 +22,9 @@ class MACHRACE_API UAutopilot : public UActorComponent
 
 private:
 
-	
+	float lastDelta = 0.0;
 	void keepUpWithTarget();
+	FVector getTargetVelocity(); // calculates target velocity (as in velocity toward which autopilot interps to)
 
 public:	
 	// Sets default values for this component's properties
@@ -29,6 +39,12 @@ public:
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "FollowTarget", Keywords = "Follow target, accelerating and decelerating as necessary."), Category = "MachRace|Gameplay")
 	void AdjustVelocityToFollowTarget(float delta);
 
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Scan", Keywords = "Scans around the owner."), Category = "MachRace|Gameplay")
+	void Scan();
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Update Velocity", Keywords = "Interpolates velocity once from current to target velocity."), Category = "MachRace|Gameplay")
+	void UpdateVelocity();
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MachRace|Gameplay")
 	float NavigationSpeed = 1.0;
 
@@ -36,10 +52,35 @@ public:
 	bool FollowTarget = false;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MachRace|Gameplay")
-	float FllowDistance = -1000;
+	float MaxFollowSpeed = -2000.0;
 
-	UPROPERTY(BlueprintReadOnly, Category = "MachRace|System")
-	FVector Velocity = FVector(0, 0, 0);
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MachRace|Gameplay")
+	float FollowOffset = -1000.0;
+
+	UPROPERTY(EditAnywhere, Category = "MachRace|Gameplay")
+	UCurveFloat* FollowOffsetApproachCurve;
+
+	UPROPERTY(BlueprintReadOnly, Category = "MachRace|Gameplay")
+	FVector Velocity = FVector::ZeroVector;
+
+	UPROPERTY(BlueprintReadOnly, Category = "MachRace|Gameplay")
+	FVector TargetFollowLocation = FVector::ZeroVector;
+
+	UPROPERTY(BlueprintReadOnly, Category = "MachRace|Gameplay")
+	FVector DodgeVelocity = FVector::ZeroVector;
+
+	UPROPERTY(BlueprintReadOnly, Category = "MachRace|Gameplay")
+	FVector TargetFollowVelocity = FVector::ZeroVector;
+
+	UPROPERTY(BlueprintReadOnly, Category = "MachRace|Gameplay")
+	FVector AlignWithTargetVelocity = FVector::ZeroVector;
+
+	// not velocity of the target, but rather desired velocity that autopilot adjusts to with every UpdateVelocity()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MachRace|System")
+	FVector TargetVelocity; 
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MachRace|Gameplay")
+	float VelocityChangeSpeed = 5.0;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MachRace|System")
 	AActor* Target;
